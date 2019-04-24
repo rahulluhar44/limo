bfaf0ae81653bcee4afdff90368de61458981b79
dell <dell@Abhishek>
2019-04-24 18:58:24 +0530

    commit2

---
+++ chromeTest/src/test/java/om/abc/maven/chromeTest/test.java
@@ -0,0 +1,13 @@
+package om.abc.maven.chromeTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class test {
+	@Test
+	public void ctest() {
+		ChromeDriver driver=new ChromeDriver();
+	}
+
+}
+
